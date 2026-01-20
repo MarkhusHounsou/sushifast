9338abe67ada0cd59e4bfd694488f1d523ed03bb
Markhus <hounsoumarkhus2004@gmail.com>
2026-01-20 22:04:12 +0100

    d

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-const isProduction = process.env.NODE_ENV === "production";
-
 export default defineConfig({
   plugins: [react()],
-  base: isProduction ? "/sushifast/" : "/",
+  base: "/sushifast/",
 });
